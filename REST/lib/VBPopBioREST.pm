bf97fc6bf474d5fa1b0ce7620683a03e435bc66c
Bob MacCallum <uncoolbob@gmail.com>
2013-05-08 23:25:22 +0100

    memcached support added to Dancer REST app

--- REST/lib/VBPopBioREST.pm
+++ REST/lib/VBPopBioREST.pm
@@ -3,6 +3,7 @@ use Dancer::Plugin::DBIC 'schema';
 use Dancer ':syntax';
 use lib '../api/Bio-Chado-VBPopBio/lib';
 use Bio::Chado::VBPopBio;
+use Dancer::Plugin::MemcachedFast;
 
 our $VERSION = '0.1';
 
@@ -29,223 +30,215 @@ get '/' => sub{
 # Project
 get qr{/project/(\w+)(/head)?} => sub {
     my ($id, $head) = splat;
-    my $project = schema->projects->find_by_stable_id($id);
+    memcached_get_or_set("project/$id$head", sub {
+			   my $project = schema->projects->find_by_stable_id($id);
+			   if (defined $project) {
+			     return $project->as_data_structure(defined $head ? 0 : undef);
+			   } else {
+			     return { error_message => "can't find project" };
+			   }
+			 });
+  };
 
-    if (defined $project) {
-	return $project->as_data_structure(defined $head ? 0 : undef);
-    } else {
-	return { error_message => "can't find project" };
-    }
-};
-
-#Projects
+# Projects
 get qr{/projects(/head)?} => sub {
-    
     my ($head) = splat;
-    my $depth = $head ? 0 : undef;
     my $l = params->{l} || 20;
-    my $o = params->{o} || 0;    
-    
-    
-    my $results = schema->projects->search(
-	undef,
-	{
-	    rows => $l,
-	    offset => $o,
-	    page => 1,
-	},
-	);
+    my $o = params->{o} || 0;
 
-    return {
-	    records => [ map { $_->as_data_structure($depth) } $results->all ],
-	    records_info($o, $l, $results)
-    }
-    
-};
+    memcached_get_or_set("projects$head-$o-$l", sub {
+			   my $results = schema->projects->search(
+								  undef,
+								  {
+								   rows => $l,
+								   offset => $o,
+								   page => 1,
+								  },
+								 );
+			   my $depth = $head ? 0 : undef;
+			   return {
+				   records => [ map { $_->as_data_structure($depth) } $results->all ],
+				   records_info($o, $l, $results)
+				  };
+			 });
+  };
 
-
-#Stocks
+# Stocks
 get qr{/(?:stocks|samples)(/head)?} => sub {
     my ($head) = splat;
-    my $depth = $head ? 0 : undef;
     my $l = params->{l} || 20;
-    my $o = params->{o} || 0;    
-    
-    my $results = schema->stocks->search(
-	undef,
-	{
-	    rows => $l,
-	    offset => $o,
-	    page => 1,
-	},
-	);
-    
-    return {
-	records => [ map { $_->as_data_structure($depth) } $results->all ],
-	records_info($o, $l, $results),
-    }
-};
+    my $o = params->{o} || 0;
 
-#Stock
+    memcached_get_or_set("samples$head-$o-$l", sub {
+			   my $results = schema->stocks->search(
+								undef,
+								{
+								 rows => $l,
+								 offset => $o,
+								 page => 1,
+								},
+							       );
+			   my $depth = $head ? 0 : undef;
+			   return {
+				   records => [ map { $_->as_data_structure($depth) } $results->all ],
+				   records_info($o, $l, $results),
+				  };
+			 })
+  };
+
+# Stock
 get qr{/(?:stock|sample)/(\w+)(/head)?} => sub {
     my ($id, $head) = splat;
-    my $stock = schema->stocks->find_by_stable_id($id);
 
-    if (defined $stock) {
-	return $stock->as_data_structure(defined $head ? 0 : undef);
-    } else {
-	return { error_message => "can't find stock" };
-    }
-};
+    memcached_get_or_set("sample/$id$head", sub {
+			   my $stock = schema->stocks->find_by_stable_id($id);
 
-#Assay
+			   if (defined $stock) {
+			     return $stock->as_data_structure(defined $head ? 0 : undef);
+			   } else {
+			     return { error_message => "can't find stock" };
+			   }
+			 });
+  };
+
+# Assay
 get qr{/assay/(\w+)(/head)?} => sub {
     my ($id, $head) = splat;
-    my $assay = schema->experiments->find_by_stable_id($id);
 
-    if (defined $assay) {
-	return $assay->as_data_structure(defined $head ? 0 : undef);
-    } else {
-	return { error_message => "can't find assay" };
-    }
-};
+    memcached_get_or_set("assay/$id$head", sub {
+			   my $assay = schema->experiments->find_by_stable_id($id);
 
-#Project/stocks
+			   if (defined $assay) {
+			     return $assay->as_data_structure(defined $head ? 0 : undef);
+			   } else {
+			     return { error_message => "can't find assay" };
+			   }
+			 });
+  };
+
+# Project/stocks
 get qr{/project/(\w+)/(?:stocks|samples)(/head)?} => sub {
     my ($id, $head) = splat;
-    my $project = schema->projects->find_by_stable_id($id);
     my $l = params->{l} || 20;
     my $o = params->{o} || 0;
 
-    my $stocks = $project->stocks->search(
-	undef,
-	{
-	    rows => $l,
-	    offset => $o,
-	    page => 1,
-	},
-	);
+    memcached_get_or_set("project/$id/samples$head-$o-$l", sub {
+			   my $project = schema->projects->find_by_stable_id($id);
 
-    return {
-	records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $stocks->all ],
-	records_info($o, $l, $stocks)
-    };
-};
+			   my $stocks = $project->stocks->search(
+								 undef,
+								 {
+								  rows => $l,
+								  offset => $o,
+								  page => 1,
+								 },
+								);
 
-#assay/stocks
+			   return {
+				   records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $stocks->all ],
+				   records_info($o, $l, $stocks)
+				  };
+			 });
+  };
+
+# assay/stocks
 get qr{/assay/(\w+)/(?:stocks|samples)(/head)?} => sub {
     my ($id, $head) = splat;
-    my $assay = schema->experiments->find_by_stable_id($id);
     my $l = params->{l} || 20;
     my $o = params->{o} || 0;
 
-    my $stocks = $assay->stocks->search(
-	undef,
-	{
-	    rows => $l,
-	    offset => $o,
-	    page => 1,
-	},
-	);
+    memcached_get_or_set("assay/$id/samples$head-$o-$l", sub {
+			   my $assay = schema->experiments->find_by_stable_id($id);
 
-    return {
-	records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $stocks->all ],
-	records_info($o, $l, $stocks)
-    };
+			   my $stocks = $assay->stocks->search(
+							       undef,
+							       {
+								rows => $l,
+								offset => $o,
+								page => 1,
+							       },
+							      );
+
+			   return {
+				   records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $stocks->all ],
+				   records_info($o, $l, $stocks)
+				  };
+			 });
 };
 
-#assay/projects
+# assay/projects
 get qr{/assay/(\w+)/projects(/head)?} => sub {
     my ($id, $head) = splat;
-    my $assay = schema->experiments->find_by_stable_id($id);
     my $l = params->{l} || 20;
     my $o = params->{o} || 0;
 
-    my $projects = $assay->projects->search(
-	undef,
-	{
-	    rows => $l,
-	    offset => $o,
-	    page => 1,
-	},
-	);
+    memcached_get_or_set("assay/$id/projects$head-$o-$l", sub {
+			   my $assay = schema->experiments->find_by_stable_id($id);
 
-    return {
-	records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $projects->all ],
-	records_info($o, $l, $projects)
-    };
-};
+			   my $projects = $assay->projects->search(
+								   undef,
+								   {
+								    rows => $l,
+								    offset => $o,
+								    page => 1,
+								   },
+								  );
 
+			   return {
+				   records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $projects->all ],
+				   records_info($o, $l, $projects)
+				  };
+			 });
+  };
 
-## JUST FOR DEMO/TESTING
-#get '/organisms' => sub {
-#
-#    my $result = schema->organisms->search(
-#	undef,
-#	{ 
-#	    rows => params->{l} || 20,
-#	    offset => params->{o} || 0,
-#   
-#        },
-#	);
-#
-#    return { records => [ map { $_->as_data_structure } $result->all ] };   
-#
-#};
-
-#Stock/projects
+# Stock/projects
 get qr{/(?:stock|sample)/(\w+)/projects(/head)?} => sub {
     my ($id, $head) = splat;
-    my $stock = schema->stocks->find_by_stable_id($id);
     my $l = params->{l} || 20;
     my $o = params->{o} || 0;
 
-    my $projects = $stock->projects->search(
-	undef,
-	{
-	    rows => $l,
-	    offset => $o,
-	    page => 1,
-	},
-	);
+    memcached_get_or_set("stock/$id/projects$head-$o-$l", sub {
+			   my $stock = schema->stocks->find_by_stable_id($id);
+			   my $projects = $stock->projects->search(
+								   undef,
+								   {
+								    rows => $l,
+								    offset => $o,
+								    page => 1,
+								   },
+								  );
 
-    my $count = $projects->count;
+			   return {
+				   records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $projects->all ],
+				   records_info($o, $l, $projects)
+				  };
+			 });
+  };
 
-    return {
-	records => [ map { $_->as_data_structure(defined $head ? 0 : undef) } $projects->all ],
-	records_info($o, $l, $projects)
-    };
-};
-
-#Stock/assays
+# Stock/assays
 get qr{/(?:stock|sample)/(\w+)/assays} => sub {
     my ($id) = splat;
-    my $stock = schema->stocks->find_by_stable_id($id);
-    
-    
-    my $o = params->{o} || 0; 
+    my $o = params->{o} || 0;
     my $l = params->{l} || 20;
-    
-    
-    my $experiments = $stock->experiments->search(
-	undef,
-	{
-	    rows => $l,
-	    offset => $o,
-	    page => 1,
-	},
-	);
-    
-    my $count = $experiments->count;
-    
-    
-    return {
-	records => [ map { $_->as_data_structure } $experiments->all ],
-	records_info($o, $l, $experiments)
-    };
-};
 
+    memcached_get_or_set("stock/$id/assays-$o-$l", sub {
+			   my $stock = schema->stocks->find_by_stable_id($id);
+			   my $experiments = $stock->experiments->search(
+									 undef,
+									 {
+									  rows => $l,
+									  offset => $o,
+									  page => 1,
+									 },
+									);
+			   return {
+				   records => [ map { $_->as_data_structure } $experiments->all ],
+				   records_info($o, $l, $experiments)
+				  };
+			 });
+  };
 
+# fall back 404
 any qr{.*} => sub {
         warning "Cannot resolve request path '".request->path."'";
         return { not_found => request->path };
@@ -258,11 +251,9 @@ any qr{.*} => sub {
 sub records_info {
     my ($o, $l, $page) = @_;
     my $end = $o + $page->count;
-    
     return (
-	start => $o + 1, 
+	start => $o + 1,
 	end => $end,
-	
 	# have to do the following because $page->count returns page size
 	count => $page->pager->total_entries,
 	);
