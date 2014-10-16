ce928bda8bb90c0fa19fa94ade86fd87457a6396
bobular <uncoolbob@gmail.com>
2014-10-16 15:27:21 +0100

    new GenBank linkout

--- www/tt-sources/src/js/config.js
+++ www/tt-sources/src/js/config.js
@@ -9,7 +9,8 @@ configTxt = {
 	'MR4 accession' : 'http://www.mr4.org/MR4ReagentsSearch/Results.aspx?BEINum=####&Template=livingMosquitoes',
 	'BioSamples accession' : 'http://www.ebi.ac.uk/biosamples/sample/####',
         'UCDavis ID' : 'https://grassi2.ucdavis.edu/PopulationData/DataViews/indiv.php?id=####',
-	'PubMed ID' : 'http://www.ncbi.nlm.nih.gov/pubmed/####'
+	'PubMed ID' : 'http://www.ncbi.nlm.nih.gov/pubmed/####',
+	'GenBank ID' : 'http://www.ncbi.nlm.nih.gov/nuccore/####'
     }
 };
 
