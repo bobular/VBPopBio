dfc55227be689b8d73f2bce59edd25dd2449a8df
bobular <uncoolbob@gmail.com>
2015-04-07 21:05:47 +0100

    changed UC Davis linkout for VB-3553

--- www/tt-sources/src/js/config.js
+++ www/tt-sources/src/js/config.js
@@ -8,7 +8,7 @@ configTxt = {
     'linkouts' : {
 	'MR4 accession' : 'http://www.mr4.org/MR4ReagentsSearch/Results.aspx?BEINum=####&Template=livingMosquitoes',
 	'BioSamples accession' : 'http://www.ebi.ac.uk/biosamples/sample/####',
-        'UCDavis ID' : 'https://grassi2.ucdavis.edu/PopulationData/DataViews/indiv.php?id=####',
+        'UCDavis ID' : 'https://popi.ucdavis.edu/PopulationData/DataViews/indiv.php?id=####',
 	'PubMed ID' : 'http://www.ncbi.nlm.nih.gov/pubmed/####',
 	'GenBank ID' : 'http://www.ncbi.nlm.nih.gov/nuccore/####'
     }
