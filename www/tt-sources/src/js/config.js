24ef54ee32685de79e733dc829938afe422de5d5
bobular <uncoolbob@gmail.com>
2016-10-12 17:01:26 +0100

    updated MR4 linkout for VB-5523

--- www/tt-sources/src/js/config.js
+++ www/tt-sources/src/js/config.js
@@ -6,7 +6,7 @@ configTxt = {
     'VBROOT':'[% vb_root %]',
     'ROOT_STATIC':'[% root_static %]',
     'linkouts' : {
-	'MR4 accession' : 'http://www.mr4.org/MR4ReagentsSearch/Results.aspx?BEINum=####&Template=livingMosquitoes',
+	'MR4 accession' : 'https://www.beiresources.org/Catalog/BEINucleicAcids/####.aspx',
 	'BioSamples accession' : 'http://www.ebi.ac.uk/biosamples/sample/####',
         'UCDavis ID' : 'https://popi.ucdavis.edu/PopulationData/DataViews/indiv.php?id=####',
 	'PubMed ID' : 'http://www.ncbi.nlm.nih.gov/pubmed/####',
