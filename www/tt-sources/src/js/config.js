fbc80a8d7a83b38930183218a8769ecd41fdee5a
bobular <uncoolbob@gmail.com>
2017-04-25 00:53:18 +0100

    added Malariagen linkout

--- www/tt-sources/src/js/config.js
+++ www/tt-sources/src/js/config.js
@@ -10,7 +10,8 @@ configTxt = {
 	'BioSamples accession' : 'http://www.ebi.ac.uk/biosamples/sample/####',
         'UCDavis ID' : 'https://popi.ucdavis.edu/PopulationData/DataViews/indiv.php?id=####',
 	'PubMed ID' : 'http://www.ncbi.nlm.nih.gov/pubmed/####',
-	'GenBank ID' : 'http://www.ncbi.nlm.nih.gov/nuccore/####'
+	'GenBank ID' : 'http://www.ncbi.nlm.nih.gov/nuccore/####',
+	'Oxford identifier' : 'https://www.malariagen.net/apps/ag1000g/phase1-AR3/index.html?dataset=Ag1000G&Ox_code=#####table_samples'
     }
 };
 
