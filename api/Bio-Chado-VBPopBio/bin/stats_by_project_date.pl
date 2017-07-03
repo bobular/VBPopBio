19491c3e088ab6a5caf843b9da3d15defc423602
bobular <uncoolbob@gmail.com>
2017-07-03 22:14:38 +0100

    added final column with project_ids

--- api/Bio-Chado-VBPopBio/bin/stats_by_project_date.pl
+++ api/Bio-Chado-VBPopBio/bin/stats_by_project_date.pl
@@ -44,6 +44,7 @@ my %date2collectioncount;
 my %date2phenotypecount;
 my %date2genotypecount;
 my %date2projectcount;
+my %date2projectids; # {date} => {project_id} => 1
 
 while (my $project = $projects->next) {
   my $stable_id = $project->stable_id;
@@ -60,19 +61,20 @@ while (my $project = $projects->next) {
   $date2collectioncount{$date} += $n_collections;
   $date2phenotypecount{$date} += $n_phenotypes;
   $date2genotypecount{$date} += $n_genotypes;
-
+  $date2projectids{$date}{$stable_id} = 1;
 }
 
 my ($sum_samples, $sum_collections, $sum_phenotypes, $sum_genotypes, $sum_projects) = (0,0,0,0,0);
 
-print "date\tprojects\tcollections\tsamples\tphenotypes\tgenotypes\n";
+print "date\tprojects\tcollections\tsamples\tphenotypes\tgenotypes\tproject_ids\n";
 
 foreach my $date (sort keys %date2samplecount) {
-  printf "%s\t%d\t%d\t%d\t%d\t%d\n",
+  printf "%s\t%d\t%d\t%d\t%d\t%d\t%s\n",
     $date,
       ($sum_projects += $date2projectcount{$date}),
 	($sum_collections += $date2collectioncount{$date}),
 	  ($sum_samples += $date2samplecount{$date}),
 	    ($sum_phenotypes += $date2phenotypecount{$date}),
-	      ($sum_genotypes += $date2genotypecount{$date});
+	      ($sum_genotypes += $date2genotypecount{$date}),
+		join(",", sort keys %{$date2projectids{$date}});
 }
