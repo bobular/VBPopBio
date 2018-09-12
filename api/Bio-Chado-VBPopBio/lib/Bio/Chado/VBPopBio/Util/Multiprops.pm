f41a8aad5d4781bcb34656e6235405efde509bd6
bobular <uncoolbob@gmail.com>
2018-09-12 16:59:50 +0100

    improved testing and functionality of duplicate multiprop detection

--- api/Bio-Chado-VBPopBio/lib/Bio/Chado/VBPopBio/Util/Multiprops.pm
+++ api/Bio-Chado-VBPopBio/lib/Bio/Chado/VBPopBio/Util/Multiprops.pm
@@ -95,6 +95,75 @@ sub add_multiprop {
   return $multiprop;
 }
 
+=head2 delete_multiprop
+
+If it successfully deletes an exact copy of the multiprop passed to it, then
+it will return the multiprop object, otherwise undef.,
+
+hash args: row => DBIx::Class Row or Result object
+           prop_relation_name => DBIx props table relation name, e.g. 'stockprops'
+           multiprop => Multiprop object
+
+=cut
+
+sub delete_multiprop {
+  my ($class, %args) = @_;
+
+  return undef;
+
+  # check for required args
+  $args{$_} or confess "must provide $_ arg"
+    for qw/row prop_relation_name multiprop/;
+
+  my $row = delete $args{row};
+  my $multiprop = delete $args{multiprop};
+  my $prop_relation_name = delete $args{prop_relation_name};
+
+  %args and confess "invalid option(s): ".join(', ', sort keys %args);
+
+  # perform the (expensive!) check for existing multiprops
+  my $input_json = undef;
+  foreach my $existing_multiprop ($row->multiprops) {
+    $input_json //= $multiprop->as_json; # only make $input_json if there are existing multiprops
+    return $existing_multiprop if ($existing_multiprop->as_json eq $input_json);
+  }
+
+  # find the highest rank of existing props
+  my $max_rank = $row->$prop_relation_name->get_column('rank')->max;
+
+  # ignore negative ranks and default to zero
+  $max_rank = 0 unless (defined $max_rank && $max_rank > 0);
+
+  # assign next available rank for the first cvterm of the new multiprop
+  my $rank = $max_rank + 1;
+
+  defined $multiprop->rank and confess "predefined rank not yet handled in add_multiprop";
+
+  # set the rank on the passed object in case the caller wants to
+  $multiprop->rank($rank);
+
+  my $last_prop; # keep track so we can add the value if needed
+  foreach my $cvterm ($multiprop->cvterms) {
+    $last_prop = $row->create_related($prop_relation_name,
+					      { type => $cvterm,
+						rank => $rank++,
+						value => $MAGIC_VALUE # subject to change below
+					      });
+  }
+
+  # if value is undef, then we will terminate the chain with NULL in database
+  # if it's a comma then that means the chain continues (comma == MAGIC VALUE)
+  # if it's a non-comma value that also terminates the chain
+  my $value = $multiprop->value;
+  confess "magic value '$MAGIC_VALUE' is not allowed as a multiprop value"
+    if (defined $value && $value eq $MAGIC_VALUE);
+  $last_prop->value($value);
+  $last_prop->update();
+
+  return $multiprop;
+}
+
+
 =head2 get_multiprops
 
 Retrieve props and process them into multiprops
