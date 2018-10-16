8cf5ef5f3a532be6a5cb6cfd4acf8b658be14be8
bobular <uncoolbob@gmail.com>
2018-10-16 12:48:56 +0100

    new project_add_tags.pl and fixed up attractants script

--- api/Bio-Chado-VBPopBio/bin/project_add_tags.pl
+++ api/Bio-Chado-VBPopBio/bin/project_add_tags.pl
@@ -2,11 +2,11 @@
 # -*- mode: cperl -*-
 #
 #
-# usage: bin/project_add_attractants.pl --project VBP0000nnn IRO:nnnnnnn IRO:mmmmmmmm ...
+# usage: bin/project_add_tags.pl --project VBP0000nnn VBcv:nnnnnnn VBcv:mmmmmmmm ...
 #
 #        also allows comma-separated project IDs.
 #
-# adds attractants to EVERY collection assay, no questions asked...
+# adds tag(s) to the project(s) given, no questions asked
 #
 # options:
 #   --dry-run              : rolls back transaction and doesn't insert into db permanently
@@ -40,9 +40,6 @@ my @term_accs = @ARGV;
 
 $schema->txn_do_deferred
   ( sub {
-
-      my $attractant_heading = $schema->types->attractant;
-
       my @terms = map { $_ =~ /^(\w+):(\d+)$/ ?
 			  $cvterms->find_by_accession({ term_source_ref => $1, term_accession_number => $2}) : () } @term_accs;
 
@@ -56,13 +53,12 @@ $schema->txn_do_deferred
 	  next;
 	}
 
-	$project->update_modification_date() if ($project);
-
-	foreach my $collection ($project->field_collections) {
-	  foreach my $term (@terms) {
-	    $collection->add_multiprop(Multiprop->new(cvterms=>[$attractant_heading, $term]));
-	  }
+	my $changed_something;
+	foreach my $term (@terms) {
+	  my $result = $project->add_tag($term);
+	  $changed_something = 1 if ($result);
 	}
+	$project->update_modification_date() if ($changed_something);
       }
 
       $schema->defer_exception("dry-run option - rolling back") if ($dry_run);
