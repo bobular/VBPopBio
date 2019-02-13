0a9c49184c506dd58a7a218c0c0436e0e0417c4c
bobular <uncoolbob@gmail.com>
2019-02-13 15:26:27 +0000

    added multi project option

--- api/Bio-Chado-VBPopBio/bin/truncate_geo_coordinates.pl
+++ api/Bio-Chado-VBPopBio/bin/truncate_geo_coordinates.pl
@@ -3,6 +3,8 @@
 #
 # usage: bin/truncate_gps_coordinates.pl --project VBP0000nnn --format %.2f
 #
+# comma-separated projects also allowed
+#
 # options:
 #
 # --dry-run
@@ -28,47 +30,53 @@ my $dbname = $ENV{CHADO_DB_NAME};
 my $dbuser = $ENV{USER};
 my $dry_run;
 my $format;
-my $project_id;
+my $project_ids;
 
 GetOptions("dbname=s"=>\$dbname,
 	   "dbuser=s"=>\$dbuser,
 	   "dry-run|dryrun"=>\$dry_run,
-	   "project=s"=>\$project_id,
+	   "projects=s"=>\$project_ids,
 	   "format=s"=>\$format,
 	  );
 
 
-die "must provide --format and --project args\n" unless ($format && $project_id);
+die "must provide --format and --project args\n" unless ($format && $project_ids);
 
 my $dsn = "dbi:Pg:dbname=$dbname";
 my $schema = Bio::Chado::VBPopBio->connect($dsn, $dbuser, undef, { AutoCommit => 1 });
 # the next line is for extra speed - but check for identical results with/without
 $schema->storage->_use_join_optimizer(0);
 my $stocks = $schema->stocks;
-my $project = $schema->projects->find_by_stable_id($project_id);
-
-die "not a valid project ID '$project_id'\n" unless ($project);
-
-warn "processing ".$project->name."...\n";
 
 # should speed things up
 $schema->storage->_use_join_optimizer(0);
 
 $schema->txn_do_deferred
   ( sub {
-      my $collections = $project->field_collections;
+      foreach my $project_id (split /\W+/, $project_ids) {
 
-      while (my $collection = $collections->next) {
-	my $geolocation = $collection->geolocation;
-	if ($geolocation) {
-	  my $latitude = $geolocation->latitude;
-	  my $longitude = $geolocation->longitude;
-	  if (looks_like_number($latitude) && looks_like_number($longitude)) {
-	    $geolocation->latitude(sprintf $format, $latitude);
-	    $geolocation->longitude(sprintf $format, $longitude);
-	    $geolocation->update;
-	  }
-	}
+        my $project = $schema->projects->find_by_stable_id($project_id);
+        unless ($project) {
+          $schema->defer_exception("not a valid project ID '$project_id'");
+          next;
+        }
+
+        warn "processing ".$project->name."...\n";
+
+        my $collections = $project->field_collections;
+
+        while (my $collection = $collections->next) {
+          my $geolocation = $collection->geolocation;
+          if ($geolocation) {
+            my $latitude = $geolocation->latitude;
+            my $longitude = $geolocation->longitude;
+            if (looks_like_number($latitude) && looks_like_number($longitude)) {
+              $geolocation->latitude(sprintf $format, $latitude);
+              $geolocation->longitude(sprintf $format, $longitude);
+              $geolocation->update;
+            }
+          }
+        }
       }
 
       $schema->defer_exception("dry-run option - rolling back") if ($dry_run);
