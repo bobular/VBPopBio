ab5f070ce3cb6c3ebef49df3b44b3542707e41fd
bobular <uncoolbob@gmail.com>
2019-07-11 16:03:51 +0100

    another fix

--- api/Bio-Chado-VBPopBio/bin/create_json_for_solr.pl
+++ api/Bio-Chado-VBPopBio/bin/create_json_for_solr.pl
@@ -547,13 +547,13 @@ while (my $stock = $stocks->next) {
   fallback_value($document->{attractants_cvterms}, 'no data');
 
   $document->{geolocation_provenance_s} //= 'no data';
-  fallback_value($document->{geolocation_provenance_cvterms}, 'no data');
+  $document->{geolocation_provenance_cvterms} //= [ 'no data' ];
 
   $document->{geolocation_accuracy_s} //= 'no data';
-  fallback_value($document->{geolocation_accuracy_cvterms}, 'no data');
+  $document->{geolocation_accuracy_cvterms} //= [ 'no data' ];
 
   $document->{geolocation_precision_s} //= 'no data';
-  fallback_value($document->{geolocation_precision_cvterms}, 'no data');
+  $document->{geolocation_precision_cvterms} //= [ 'no data' ];
 
   #
   # handle data signposting (only for non-zero samples)
