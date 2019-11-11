c194ad4c63d07f1a7bffc59e5f37f6e906a5b3a9
bobular <uncoolbob@gmail.com>
2019-11-11 20:31:41 +0000

    added dataset_names placeholder comment for test-loads

--- api/Bio-Chado-VBPopBio/lib/Bio/Chado/VBPopBio/Result/Stock.pm
+++ api/Bio-Chado-VBPopBio/lib/Bio/Chado/VBPopBio/Result/Stock.pm
@@ -530,6 +530,7 @@ sub as_isatab {
 	   study_assay_measurement_type_term_accession_number => $assay->type->dbxref->accession,
 	   study_assay_file_name => $assay_filename,
 	   samples => ordered_hashref(),
+           comments => { dataset_names => 'VB-PopBio-test|1.0' },
 	  };
 
     my $assay_name = $assay->external_id;
