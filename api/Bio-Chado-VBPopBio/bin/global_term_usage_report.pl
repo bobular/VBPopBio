0d2838755c82eb07c8ad96d12368cd5f3de264bb
bobular <uncoolbob@gmail.com>
2020-01-09 00:02:50 +0000

    changed quite a bit to also output relationships back to the root term

--- api/Bio-Chado-VBPopBio/bin/global_term_usage_report.pl
+++ api/Bio-Chado-VBPopBio/bin/global_term_usage_report.pl
@@ -4,8 +4,8 @@
 #
 # usage: CHADO_DB_NAME=my_chado_instance bin/global_term_usage_report.pl > term_usage.tsv
 #
-# option: --output foo.tsv    # tab delimited file for loading into GUS via InsertOntologyFromTabDelim.pm
-#
+# option: --terms-file  terms.tsv           # tab delimited files for loading
+#         --rels-file   relationships.tsv   # into GUS via InsertOntologyFromTabDelim.pm
 #
 
 
@@ -19,10 +19,15 @@ use Getopt::Long;
 
 my $fill_cvtermpath;
 my $tab_delim_file;
+my $relationships_file;
 
 GetOptions("prefill_cvtermpath|fill_cvtermpath"=>\$fill_cvtermpath,
-           "output=s"=>\$tab_delim_file,
+           "output|terms_file|terms-file=s"=>\$tab_delim_file,
+           "rels_file|rels-file=s"=>\$relationships_file,
           );
+
+$fill_cvtermpath = 1 if ($relationships_file);
+die "must have --terms and --rels options together\n" if ($relationships_file and not $tab_delim_file);
 
 my $dsn = "dbi:Pg:dbname=$ENV{CHADO_DB_NAME}";
 my $schema = Bio::Chado::VBPopBio->connect($dsn, $ENV{USER}, undef, { AutoCommit => 1 });
@@ -41,10 +46,6 @@ my @cvterm_relationships = grep !/^(cvterm)/, $cvterms_rs->relationships;
 # remember all unique cvterms for cvtermpath prefilling
 my %seen_cvterm_ids;
 
-
-if ($tab_delim_file) {
-  open(TAB, ">$tab_delim_file") || die;
-}
 
 print join("\t", 'Term accession prefix', 'Term accession', 'Term name', 'Number of uses in this context',  'Object type', 'Object column')."\n";
 
@@ -82,25 +83,12 @@ foreach my $relationship (@cvterm_relationships) {
 	my $accession = $dbxref->as_string;
 	my $prefix = $dbxref->db->name;
 	my $count = $cvterm->search_related($relationship)->count;
-
 	print join("\t", $prefix, $accession, $cvterm->name, $count, $obj_class, $object_column)."\n";
-
-        if ($tab_delim_file) {
-          # Tab delimited text file with the following header (order matters):
-          # id, name, def, synonyms (comma-separated), uri, is_obsolete [true/false]
-          my $source_id = $dbxref->accession;
-          print TAB join("\t", $source_id, $cvterm->name, $cvterm->definition || '',
-                         join(",", $cvterm->cvtermsynonyms->get_column('synonym')->all),
-                         "http://purl.obolibrary.org/obo/$source_id", 'false')."\n";
-        }
       }
     }
   }
 }
 
-if ($tab_delim_file) {
-  close(TAB);
-}
 
 if ($fill_cvtermpath) {
   warn "prefilling cvtermpaths...\n";
@@ -112,4 +100,54 @@ if ($fill_cvtermpath) {
   my $default_license = $cvterms->find_by_accession({ term_source_ref => 'VBcv',
                                                       term_accession_number => '0001107' }) || die;
   $default_license->recursive_parents();
+}
+
+
+if ($tab_delim_file) {
+  open(TAB, ">$tab_delim_file") || die;
+
+  if ($relationships_file) {
+    open(REL, ">$relationships_file") || die;
+  }
+
+  foreach my $cvterm_id (keys %seen_cvterm_ids) {
+    # print term and optionally recurse to all parents
+    print_term($cvterm_id);
+  }
+
+
+  if ($relationships_file) {
+    close(REL);
+  }
+
+  close(TAB);
+}
+
+
+my %printed_terms; # key is cvterm_id
+
+sub print_term {
+  my ($cvterm_id) = @_;
+
+  # don't do anything if this term has already been processed
+  return if ($printed_terms{$cvterm_id}++);
+
+  my $cvterm = $cvterms->find($cvterm_id);
+  my $dbxref = $cvterm->dbxref;
+  # Tab delimited text file with the following header (order matters):
+  # id, name, def, synonyms (comma-separated), uri, is_obsolete [true/false]
+  my $source_id = $dbxref->accession;
+  print TAB join("\t", $source_id, $cvterm->name, $cvterm->definition || '',
+                 join(",", $cvterm->cvtermsynonyms->get_column('synonym')->all),
+                 "http://purl.obolibrary.org/obo/$source_id", 'false')."\n";
+
+  if ($relationships_file) {
+    my @parent_terms = $cvterm->direct_parents()->all;
+    foreach my $parent_term (@parent_terms) {
+      my $parent_source_id = $parent_term->dbxref->accession;
+      print REL join("\t", $source_id, 'subClassOf', $parent_source_id)."\n";
+      print_term($parent_term->cvterm_id);
+    }
+  }
+
 }
