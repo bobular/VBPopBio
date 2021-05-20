8b296a4033c30f65d083b285fe99cd3c81040023
bobular <uncoolbob@gmail.com>
2021-05-20 16:10:51 +0100

    added genotype fixing bash script

---
+++ api/Bio-Chado-VBPopBio/bin/bespoke/fix-wild-type-genotypes.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yes | bin/replace_term.pl --relationship genotypes IRO:0000112 IRO:0000012
+yes | bin/replace_term.pl --relationship genotypes IRO:0000111 IRO:0000045
+yes | bin/replace_term.pl --relationship genotypes IRO:0000110 IRO:0000044
+yes | bin/replace_term.pl --relationship genotypes IRO:0000113 IRO:0000043
+yes | bin/replace_term.pl --relationship genotypes IRO:0000114 IRO:0000041
+yes | bin/replace_term.pl --relationship genotypes IRO:0000119 IRO:0000048
+yes | bin/replace_term.pl --relationship genotypes IRO:0000118 IRO:0000009
+yes | bin/replace_term.pl --relationship genotypes IRO:0000117 IRO:0000040
+yes | bin/replace_term.pl --relationship genotypes IRO:0000146 IRO:0000156
+yes | bin/replace_term.pl --relationship genotypes IRO:0000116 IRO:0000002
+yes | bin/replace_term.pl --relationship genotypes IRO:0000120 IRO:0000042
+yes | bin/replace_term.pl --relationship genotypes IRO:0000122 IRO:0000039
+yes | bin/replace_term.pl --relationship genotypes IRO:0000121 IRO:0000038
